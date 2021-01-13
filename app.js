11a32e9fbf6971d3907e29abc861de79b920047a
aldoBangun <punk.aldo68@gmail.com>
2021-01-14 03:07:02 +0800

    inital app config

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const sessionConfiguration = {
+   secret: "test",
+   resave: true,
+   saveUninitialized: true,
+};
+
+app.set("view engine", "pug");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(session(sessionConfiguration));
+
+app.listen(PORT, console.log(`App is running on http://localhost:3000`));
