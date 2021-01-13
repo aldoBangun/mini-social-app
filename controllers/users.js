bc086666cbe8b7d76407ccf8f82744963a0df097
aldoBangun <punk.aldo68@gmail.com>
2021-01-14 03:57:47 +0800

    add users controllers

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const { register, login } = require("../models/users");
+
+const userRegistration = (req, res) => {
+   register(req.body, (err) => {
+      if (err) {
+         res.render("users/register");
+      } else {
+         res.redirect("login");
+      }
+   });
+};
+
+const userLogin = (req, res) => {
+   const { email, password } = req.body;
+
+   login(email, (err, result) => {
+      if (err) {
+         return res.render("users/login", { message: "Something went wrong" });
+      }
+
+      if (!result.length) {
+         return res.render("users/login", { message: "Invalid Email" });
+      }
+
+      if (result[0].password !== password) {
+         return res.render("users/login", { message: "Invalid Password" });
+      }
+
+      res.render("index", { isAuth: true });
+   });
+};
+
+const pageUserRegister = (req, res) => {
+   res.render("users/register");
+};
+
+const pageUserLogin = (req, res) => {
+   res.render("users/login");
+};
+
+module.exports = {
+   userRegistration,
+   userLogin,
+   pageUserRegister,
+   pageUserLogin,
+};
